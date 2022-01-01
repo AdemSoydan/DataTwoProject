efa299116c99968d80c301133cdc9e736c975cb0
AdemSoydan <ademsoydan@youcangames.com>
2022-01-01 10:18:19 +0300

    Baslangic
    3 ve 4 den devam ediyoruz

---
+++ ProjectTwo/ProjectTwo/OncelikliKuyruk.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTwo
+{
+    class OncelikliKuyruk
+    {
+        List<MahalleSinifi> mahalleler;
+
+        public void ekle(MahalleSinifi yeniMahalle)
+        {
+            mahalleler.Add(yeniMahalle);
+        }
+        public void sil()
+        {
+            MahalleSinifi enFazlaTeslimataSahipMahalle =  enFazlaTeslimatOlanMahalleyiBul();
+            mahalleler.Remove(enFazlaTeslimataSahipMahalle);
+        }
+        private MahalleSinifi enFazlaTeslimatOlanMahalleyiBul()
+        {
+            MahalleSinifi enFazlaTeslimataSahipMahalle = new MahalleSinifi("");
+            foreach (MahalleSinifi mahalle in mahalleler)
+            {
+                if (mahalle.TeslimatList.Capacity > enFazlaTeslimataSahipMahalle.TeslimatList.Capacity)
+                {
+                    enFazlaTeslimataSahipMahalle = mahalle;
+                    return mahalle;
+                }
+                else if (mahalle.TeslimatList.Capacity == enFazlaTeslimataSahipMahalle.TeslimatList.Capacity)
+                {
+                    enFazlaTeslimataSahipMahalle = mahalle;
+                    return mahalle;
+                }
+            }
+            return enFazlaTeslimataSahipMahalle;
+        }
+    }
+}
